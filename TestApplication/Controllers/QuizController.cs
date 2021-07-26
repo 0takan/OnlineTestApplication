b0c7ca459266138c2db349efeb7a237664e5acb9
Mykola Hordiienko <ipz192_gmg@student.ztu.edu.ua>
2021-07-26 03:20:48 +0300

    Realize QuizController

--- TestApplication/Controllers/QuizController.cs
+++ TestApplication/Controllers/QuizController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using QuizDataLibrary;
 using System;
 using System.Collections.Generic;
@@ -10,9 +11,24 @@ namespace TestApplication.Controllers
     public class QuizController : Controller
     {
         private readonly IQuiz _quizeRepo;
+        private readonly ILogger<QuizController> _logger;
+
+        public QuizController(IQuiz quizRepo, ILogger<QuizController> logger)
+        {
+            _quizeRepo = quizRepo;
+            _logger = logger;
+        }
+        //GET: Quiz
         public IActionResult Index()
         {
-            return View();
+            var quiz = _quizeRepo.ListAllQuizzes();
+            return View(_quizeRepo.ListAllQuizzes());
+        }
+
+        //GET: Quiz/1
+        public IActionResult Details(int id)
+        {
+            return View(_quizeRepo.GetById(id));
         }
     }
 }
